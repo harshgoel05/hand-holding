d1ac0590163b7a6d4f281075db6845f7f4a8ddbc
harshgoel05 <harshgoel05@gmail.com>
2020-08-08 12:59:01 +0530

    navbar structure changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,21 @@
+var w = window.innerWidth;
+
 document.addEventListener('scroll', function () {
 	console.log(window.scrollY);
-	if (window.scrollY > 100) {
-		document.getElementById('hhs-nav').addClass('hhs-active');
-	} else {
-		document.getElementById('hhs-nav').removeClass('hhs-active');
-		document.getElementById('hhs-nav');
+	var loc = window.location.href;
+	if (!loc.includes('services.html')) {
+		if (w < 800) {
+		} else {
+			if (window.scrollY > 100) {
+				document.getElementById('hhs-nav').classList.add('hhs-active');
+			} else {
+				document.getElementById('hhs-nav').classList.remove('hhs-active');
+			}
+		}
 	}
 });
+
+console.log(w);
+if (w < 800) {
+	document.getElementById('hhs-nav').classList.add('hhs-active');
+}
