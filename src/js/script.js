27a18dbe1ad2ba3f5658ec02dddb290282caabf4
harshgoel05 <harshgoel05@gmail.com>
2020-08-10 10:31:41 +0530

    images changed and container

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
 var w = window.innerWidth;
 
 document.addEventListener('scroll', function () {
-	console.log(window.scrollY);
 	var loc = window.location.href;
 	if (!loc.includes('services.html')) {
 		if (w < 800) {
@@ -15,7 +14,6 @@ document.addEventListener('scroll', function () {
 	}
 });
 
-console.log(w);
 if (w < 800) {
 	document.getElementById('hhs-nav').classList.add('hhs-active');
 }
